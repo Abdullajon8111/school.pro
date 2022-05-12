6032f5ed9fd997239607c5dedfbd4f3c5501bdfa
abdullajon8111 <abdullajon8111@gmail.com>
2022-05-12 12:10:01 +0500

    install backpack

---
+++ app/Models/SLocationType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SLocationType extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2022_05_12_062540_create_s_language_types_table.php
+++ database/migrations/2022_05_12_062540_create_s_language_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('s_provinces', function (Blueprint $table) {
+        Schema::create('s_language_types', function (Blueprint $table) {
             $table->id();
             $table->json('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('s_provinces');
+        Schema::dropIfExists('s_language_types');
     }
 };
